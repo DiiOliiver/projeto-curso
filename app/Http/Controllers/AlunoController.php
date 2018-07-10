005a302fba3f18d9b3930306e94eec8d95c4a72a
DiiOliiver <diego.feitosa.oliveira@hotmail.com>
2018-07-10 21:56:02 +0000

    AlunoController criado

---
+++ app/Http/Controllers/AlunoController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Aluno;
+
+use App\Http\Requests\AlunoRequest;
+
+class AlunoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $alunos = Aluno::all()->sortBy('nome');
+        return view('aluno.index',compact('alunos'));
+    }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('aluno.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(AlunoRequest $request)
+    {
+        $aluno = Aluno::create($request->all());
+        return redirect('alunos');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $aluno = Aluno::find($id);
+        return view('aluno.show', ['data' => $aluno]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $aluno = Aluno::find($id);
+        return view('aluno.edit', ['data' => $aluno]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $aluno = Aluno::find($id);
+        $aluno->fill($request->all());
+        $aluno->update();
+        return redirect('alunos');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Aluno::destroy($id);
+        return redirect('alunos');
+    }
+}
