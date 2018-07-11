c179eb062daf677530d323bac7551f75aa5676f1
DiiOliiver <diego.feitosa.oliveira@hotmail.com>
2018-07-11 21:13:52 +0000

    Atualizando .env.example e tela inicial

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Painel de controle</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    Você está logado!
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Painel de controle</h1>
+    <p class="lead">Seja bem vindo!</p>
+  </div>
+</div>@endsection
